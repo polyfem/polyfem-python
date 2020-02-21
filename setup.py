a636db4fadd82b1332e542b3a0904a74bb48a0ce
Teseo Schneider <teseo@bluewin.ch>
2020-02-21 15:34:35 -0500

    removed mkl form build

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ class CMakeBuild(build_ext):
                       '-DPOLYFEM_WITH_APPS=OFF',
                       '-DPOLYFEM_WITH_MISC=OFF',
                       '-DPOLYFEM_WITH_AMGCL=ON',
-                      '-DPOLYFEM_WITH_MKL=ON',
+                    #   '-DPOLYFEM_WITH_MKL=ON',
                       '-DPOLYFEM_WITH_SPECTRA=OFF']
 
         cfg = 'Debug' if self.debug else 'Release'

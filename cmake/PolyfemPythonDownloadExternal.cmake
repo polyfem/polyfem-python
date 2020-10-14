ae7af238a8f2edf91cbdf02218008ba97c8cd8c6
Teseo Schneider <teseo@bluewin.ch>
2020-10-14 08:52:38 -0400

    updated polyfem

--- cmake/PolyfemPythonDownloadExternal.cmake
+++ cmake/PolyfemPythonDownloadExternal.cmake
@@ -27,7 +27,7 @@ endfunction()
 function(polyfem_python_download_polyfem)
     polyfem_python_download_project(polyfem
         GIT_REPOSITORY https://github.com/polyfem/polyfem.git
-        GIT_TAG        36eeb8ed3b262430f791fe6d29e6c12df4991323
+        GIT_TAG        3fa37348d896c387e3b6253e9e030e83d0ac14fa
     )
 endfunction()
 

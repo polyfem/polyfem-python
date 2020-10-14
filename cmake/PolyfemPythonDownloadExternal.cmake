c97de09204f7200e4e6198c299b33d32b8753175
teseoch <teseo@bluewin.ch>
2020-10-14 14:26:34 -0400

    gh-action (#2)
    * gh-action
    
    * fixed setup and updated polyfem
    
    * disabled macos

--- cmake/PolyfemPythonDownloadExternal.cmake
+++ cmake/PolyfemPythonDownloadExternal.cmake
@@ -27,7 +27,7 @@ endfunction()
 function(polyfem_python_download_polyfem)
     polyfem_python_download_project(polyfem
         GIT_REPOSITORY https://github.com/polyfem/polyfem.git
-        GIT_TAG        20cc7b6d525c044b4cfe7730d5b7f52961f328eb
+        GIT_TAG        43903b617ee3439d4cfad15e3b2ea6454518dd9f
     )
 endfunction()
 
